09fe453811c6ce7da099bd3d5472814dfbaa918c
Chigik45 <127143509+Chigik45@users.noreply.github.com>
2023-11-20 12:50:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,19 +9,11 @@ app.get('/', (req, res) => {
     let episodeName = `My Little Pony: Episode ${Math.floor(Math.random() * 100) + 1}`;
     episodes.push(episodeName);
 
-    fs.writeFile('episodes.json', JSON.stringify(episodes), (err) => {
-        if (err) throw err;
-    });
-
     res.json({ episodeName: episodeName });
 });
 
 app.get('/all', (req, res) => {
-    fs.readFile('episodes.json', (err, data) => {
-        if (err) throw err;
-        let allEpisodes = JSON.parse(data);
-        res.json(allEpisodes);
-    });
+    res.json(episodes);
 });
 
 app.listen(port, () => {
